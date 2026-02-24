999c7fef699ac798ecda0a4aa68eb2c7157f9ffb
Wu-py <835015085@qq.com>
2026-02-24 15:27:11 +0800

    雪佛兰

--- spider/changyi_pc/changyi_pc/spiders/changyi_xianlutu_list_fute.py
+++ spider/changyi_pc/changyi_pc/spiders/changyi_xianlutu_list_fute.py
@@ -43,7 +43,9 @@ class ChangyiDianluLisSpider(scrapy.Spider):
 
 
     def start_requests(self):
-        pp_id = '58'
+        # pp_id = '58'
+        # pp_id = '63'
+        pp_id = '64'
         yield scrapy.Request(
             url=self.start_urls[0] + f'?pinpai_id={pp_id}',
             method='GET',
@@ -77,7 +79,7 @@ class ChangyiDianluLisSpider(scrapy.Spider):
                 callback=self.parse_year_list,
                 meta={'item': item},
             )
-            break
+            # break
 
     def parse_year_list(self, response):
         # print(response.text)
@@ -100,7 +102,7 @@ class ChangyiDianluLisSpider(scrapy.Spider):
                 meta={'item': item},
                 dont_filter=True,
             )
-            break
+            # break
 
     def parse_ziliao_list(self, response):
         # print(response.text)
@@ -146,17 +148,78 @@ class ChangyiDianluLisSpider(scrapy.Spider):
         # https://www.car388.com/system/second/tree_xians_ok.php?tid=24713&pinpai_id=58
         next_url = urljoin(response.url, url)
         # next_url = 'https://www.car388.com/system/second/tree_xians_ok.php?tid=24664&pinpai_id=58'
+        if 'tree_xians_ok' in next_url:
+            callback = self.parse_tree_xians_ok
+        elif 'xlt.htm' in next_url:
+            callback = self.parse_xlt
         yield scrapy.Request(
             url=next_url,
             method='GET',
             headers=self.headers,
             cookies=self.cookies,
-            callback=self.parse_caidan_show,
+            callback=callback,
             meta={'item': item},
             dont_filter=True,
         )
 
-    def parse_caidan_show(self, response):
+    def parse_xlt(self, response):
+        lis = response.xpath('//ul[@id="containerul"]/li')
+        for index, li in enumerate(lis, 1):
+            item = copy.deepcopy(response.meta['item'])
+            for i in self.get_items(li, item, 1, index):
+                filepath = i['filepath'].replace('showpic', 'showpic_xiang')
+                i['filepath'] = urljoin(response.url, filepath)
+                print(i)
+
+    def get_items(self, li_selector, item, level, file_index=1):
+        # li_selector 是 Scrapy 的 Selector 对象
+
+        # 1. 查找子级 ul
+        ul_selectors = li_selector.xpath('./ul')
+
+        # 2. 判断是否有子级 (SelectorList 可以直接作为布尔值判断)
+        if not ul_selectors:
+            # --- 叶子节点 (没有子 ul) ---
+            base_item = copy.deepcopy(item)
+
+            # 获取标题文本
+            title_selector = li_selector.xpath('./a/text()')
+            title = title_selector.get()  # .get() 等价于 .extract_first()
+
+            if not title:
+                return
+
+            base_item[f'title_level_{level}'] = title.strip()
+            base_item[f'index_{level}'] = file_index
+
+            # 获取链接
+            href = li_selector.xpath('./a/@href').get()
+            if href:
+                base_item['filepath'] = href.strip()
+
+            yield base_item
+        else:
+            # --- 分支节点 (有子 ul) ---
+
+            # 获取当前层级标题 (直接文本)
+            # 注意：xpath('./text()') 可能返回多个文本节点，通常取第一个或拼接
+            raw_title = li_selector.xpath('./text()').get()
+            title = raw_title.strip() if raw_title else ''
+
+            # 获取子级 li 列表
+            # ul_selectors 是列表，取第一个 ul 继续查找 li
+            lis = ul_selectors[0].xpath('./li')
+
+            for index, l in enumerate(lis, 1):
+                base_item = copy.deepcopy(item)
+                base_item[f'title_level_{level}'] = title
+                base_item[f'index_{level}'] = file_index
+
+                # 递归调用，注意加上 self
+                yield from self.get_items(l, base_item, level + 1, index)
+
+    def parse_tree_xians_ok(self, response):
+        # print(response.text)
         item = copy.deepcopy(response.meta['item'])
         url = re.search('url=(.+?)>', response.text).group(1)
         # https://www.car388.com/system/second/tree_2022.php?tid=24664&pinpai_id=58
@@ -166,12 +229,13 @@ class ChangyiDianluLisSpider(scrapy.Spider):
             method='GET',
             headers=self.headers,
             cookies=self.cookies,
-            callback=self.parse_caidan,
+            callback=self.parse_tree_2022,
             meta={'item': item, 'level':1},
             dont_filter=True,
         )
 
-    def parse_caidan(self, response):
+
+    def parse_tree_2022(self, response):
         '''
         目录页
         '''
@@ -191,7 +255,7 @@ class ChangyiDianluLisSpider(scrapy.Spider):
                         method='GET',
                         headers=self.headers,
                         cookies=self.cookies,
-                        callback=self.parse_caidan,
+                        callback=self.parse_tree_2022,
                         meta={'item': i, 'level': response.meta['level'] + 1},
                         dont_filter=True,
                     )
